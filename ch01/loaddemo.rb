26ba65f9291a224bdf830d7f0b04b6f8e1fb023a
Hengyuan <hengyuan.jiang@thoughtworks.com>
2024-07-16 16:37:56 +0800

    First Chapter

--- ch01/loaddemo.rb
+++ ch01/loaddemo.rb
@@ -1,4 +1,5 @@
 puts "This is the first (master) program file."
+# global variable in Ruby that holds the array of paths to search for scripts and binary modules by load or require
 $: << "."
 require "loadee.rb"
 puts "And back again to the first file."
